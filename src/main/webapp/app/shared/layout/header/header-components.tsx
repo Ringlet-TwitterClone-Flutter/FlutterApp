29690dd132bc90451361aeb44bbb7400ee171a11
James <james@Zipcoders-MacBook-Pro-101.local>
2023-05-04 14:03:10 -0400

    butterfly silho

--- src/main/webapp/app/shared/layout/header/header-components.tsx
+++ src/main/webapp/app/shared/layout/header/header-components.tsx
@@ -6,7 +6,7 @@ import { FontAwesomeIcon } from '@fortawesome/react-fontawesome';
 
 export const BrandIcon = props => (
   <div {...props} className="brand-icon">
-    <img src="content/images/butterfreePic.png" alt="Logo" />
+    <img src="content/images/butterflySilho.png" alt="Logo" />
   </div>
 );
 
