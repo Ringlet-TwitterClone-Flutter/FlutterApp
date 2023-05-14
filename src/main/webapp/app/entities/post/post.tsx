a97ff616fbbccc98f6f82e950becc70d97c9fb23
Nina <nina.emiko.mcnair@gmail.com>
2023-05-14 12:20:20 -0400

    Got comments working, now to work on displaying them

--- src/main/webapp/app/entities/post/post.tsx
+++ src/main/webapp/app/entities/post/post.tsx
@@ -47,7 +47,7 @@ export const Post = () => {
     };
 
     dispatch(createCommentEntity(comment));
-    navigate('/comment/new');
+    navigate('/post');
   };
 
   return (
@@ -143,54 +143,9 @@ export const Post = () => {
                     {/* Input field for comment text */}
 
                     <div className="comment-container">
-                      <div>
+                      <div id="list-of-comments">
                         {commentList && commentList.length > 0 ? (
                           <div className="card-comment">
-                            {commentList
-                              .filter(comment => comment.post && comment.post.id === post.id)
-                              .map((comment, i) => (
-                                <div key={`entity-${i}`} data-cy="entityTable" className="comment">
-                                  <div className="comment-field">
-                                    <div className="comment-value">{comment.text}</div>
-                                  </div>
-                                  <div className="comment-field">
-                                    <div className="comment-value">
-                                      {comment.createdAt ? (
-                                        <TextFormat type="date" value={comment.createdAt} format={APP_DATE_FORMAT} />
-                                      ) : null}
-                                    </div>
-                                  </div>
-                                  <div className="comment-field">
-                                    <div className="comment-value">{comment.user ? comment.user.login : ''}</div>
-                                  </div>
-                                  <div className="comment-field">
-                                    <div className="comment-label"></div>
-                                    <div className="comment-value text-end">
-                                      <div className="btn-group flex-btn-group-container">
-                                        <Button
-                                          tag={Link}
-                                          to={`/comment/${comment.id}/edit`}
-                                          size="sm"
-                                          data-cy="entityEditButton"
-                                          id="edit-button"
-                                        >
-                                          <FontAwesomeIcon icon="pencil-alt" /> <span className="d-none d-md-inline">Edit</span>
-                                        </Button>
-                                        <Button
-                                          tag={Link}
-                                          to={`/comment/${comment.id}/delete`}
-                                          color="black"
-                                          size="sm"
-                                          data-cy="entityDeleteButton"
-                                          id="delete-button"
-                                        >
-                                          <FontAwesomeIcon icon="trash" /> <span className="d-none d-md-inline">Delete</span>
-                                        </Button>
-                                      </div>
-                                    </div>
-                                  </div>
-                                </div>
-                              ))}
                             <div id="success" className="alert alert-warning comment-box">
                               <div id="comment-msg">Make a comment!</div>
                               <div className="comment-input-container">
@@ -200,6 +155,74 @@ export const Post = () => {
                                   <FontAwesomeIcon icon="plus" /> <span className="d-none d-md-inline">Comment</span>
                                 </Button>
                               </div>
+                            </div>
+                            <div id="individual-comments">
+                              {commentList
+                                .filter(comment => comment.post && comment.post.id === post.id)
+                                .map((comment, i) => (
+                                  <div key={`entity-${i}`} data-cy="entityTable" className="comment">
+                                    <div className="card">
+                                      <div className="post-list-cell post-header" id="post-header">
+                                        <div className="comment-value">{comment.user ? comment.user.login : ''}</div>
+                                      </div>
+                                      <div className="comment-field">
+                                        <div className="post-time">
+                                          {comment.createdAt ? (
+                                            <TextFormat type="date" value={comment.createdAt} format={APP_DATE_FORMAT} />
+                                          ) : null}
+                                        </div>
+                                      </div>
+                                      <div className="post-list-cell post-content" id="post-content">
+                                        <div className="comment-value">{comment.text}</div>
+                                      </div>
+
+                                      {/* {post.user &&
+                          post.user.login === currentUser.login && ( // Check if post belongs to the current user
+                            <Button
+                              tag={Link}
+                              to={`/post/${post.id}/edit`}
+                              color="primary"
+                              size="sm"
+                              data-cy="entityEditButton"
+                              id="edit-button"
+                            >
+                              <FontAwesomeIcon icon="pencil-alt" /> <span className="d-none d-md-inline">Edit</span>
+                            </Button>
+                          )} */}
+
+                                      <div className="comment-field">
+                                        <div className="comment-label"></div>
+                                        <div className="comment-value text-end">
+                                          <div className="btn-group flex-btn-group-container">
+                                            {comment.user && comment.user.login === currentUser.login && (
+                                              <Button
+                                                tag={Link}
+                                                to={`/comment/${comment.id}/edit`}
+                                                size="sm"
+                                                data-cy="entityEditButton"
+                                                id="edit-button"
+                                              >
+                                                <FontAwesomeIcon icon="pencil-alt" /> <span className="d-none d-md-inline">Edit</span>
+                                              </Button>
+                                            )}
+                                            {comment.user && comment.user.login === currentUser.login && (
+                                              <Button
+                                                tag={Link}
+                                                to={`/comment/${comment.id}/delete`}
+                                                color="black"
+                                                size="sm"
+                                                data-cy="entityDeleteButton"
+                                                id="delete-button"
+                                              >
+                                                <FontAwesomeIcon icon="trash" /> <span className="d-none d-md-inline">Delete</span>
+                                              </Button>
+                                            )}
+                                          </div>
+                                        </div>
+                                      </div>
+                                    </div>
+                                  </div>
+                                ))}
                             </div>
                           </div>
                         ) : (
